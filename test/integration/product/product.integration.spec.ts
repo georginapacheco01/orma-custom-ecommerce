9be2f9b529922075fbdcf9b024da189b5f2c21b1
Andrea <pacheco110798@gmail.com>
2026-05-05 19:22:51 -0700

    test: add products unit and integration test

---
+++ test/integration/product/product.integration.spec.ts
@@ -0,0 +1,27 @@
+import app from "../../../src/app";
+import http from "http";
+
+const PORT = 5001;
+let server: http.Server;
+
+beforeAll((done) => {
+  server = app.listen(PORT, () => {
+    done();
+  });
+});
+
+afterAll((done) => {
+  server.close(done);
+});
+
+describe("Product API Integration", () => {
+  it("should fetch products", async () => {
+    const res = await fetch(`http://localhost:${PORT}/api/products`);
+    const data = await res.json();
+
+    expect(res.status).toBe(200);
+    expect(data.success).toBe(true);
+    expect(data.data).toHaveLength(2);
+    expect(data.message).toBe("Products retrieved successfully");
+  });
+});
---
+++ test/unit/product/product.service.spec.ts
@@ -0,0 +1,18 @@
+import {
+  findProductById,
+  getAllProducts,
+} from "../../../src/services/products.service";
+describe("Design Service", () => {
+  it("should  get all products ", () => {
+    const result = getAllProducts();
+    expect(result).toHaveLength(2);
+  });
+
+  it("should retrieve a product by id", () => {
+    const expectedProduct = { id: "1", name: "Product 1" };
+    const found = findProductById("1");
+
+    expect(found).toBeDefined();
+    expect(found).toEqual(expectedProduct);
+  });
+});
