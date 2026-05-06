019ddbc60b919d37895d9bf1bbd039ba709d70dd
Andrea <pacheco110798@gmail.com>
2026-05-05 17:49:12 -0700

    refactor: separate server entrypoint from app building

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,9 @@ import express from "express";
 import routes from "./routes";
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
 app.use("/api", routes);
 
-app.listen(PORT, () => {
-  console.log("Server is running on port 3000");
-});
+export default app;
